8826657e30d4a5a3665c1c89d135a7e1fb38064a
tony <antonel-ernest.pazargic@ing.com>
2024-10-09 13:49:50 +0300

    From CF to Kotlin coroutines and the other way around.

--- src/main/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/CustomerResource.kt
+++ src/main/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/CustomerResource.kt
@@ -1,22 +1,34 @@
 package ro.jtonic.handson.spring.kotlin.coroutines
 
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.asDeferred
+import kotlinx.coroutines.future.future
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/customers")
 class CustomerResource(
-    private val customerService: CustomerService,
+    private val customerService1: CustomerService1,
+    private val customerService2: CustomerService2,
+    private val customerService3: CustomerService3,
     private val runFlow: UseCase<Input, Unit>,
 ) {
 
-    @GetMapping
-    suspend fun getCustomers(): List<Customer> =
-        customerService.getCustomers()
+    @GetMapping("/1")
+    suspend fun getCustomers1(): List<Customer> =
+        customerService1.getCustomers()
+
+    @GetMapping("/2")
+    suspend fun getCustomers2(): List<Customer> =
+        customerService2.getCustomers().asDeferred().await()
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @GetMapping("/3")
+    fun getCustomers3(): CompletableFuture<List<Customer>> =
+        GlobalScope.future { customerService3.getCustomers() }
 
     @PostMapping
     suspend fun postRunFlow(@RequestParam("input") input: Int): Unit = run {
@@ -25,13 +37,31 @@ class CustomerResource(
 }
 
 @Service
-class CustomerService {
+class CustomerService1 {
     suspend fun getCustomers() =
         listOf(
-            Customer("1", "Antonel-Ernest Pazargic", 53),
+            Customer("1", "Antonel-Ernest Pazargic", 54),
             Customer("2", "Liviu Pazargic", 40),
             Customer("3", "Irina Pazargic", 33),
         )
 }
 
+@Service
+class CustomerService2 {
+    fun getCustomers(): CompletableFuture<List<Customer>> = CompletableFuture.completedFuture(
+        listOf(
+            Customer("10", "Antonel-Ernest Pazargic", 54),
+            Customer("20", "Liviu Pazargic", 40),
+            Customer("30", "Irina Pazargic", 33),
+        )
+    )
+}
+
+@Service
+class CustomerService3 {
+    suspend fun getCustomers() =
+        listOf(
+            Customer("100", "Antonel-Ernest Pazargic", 54),
+        )
+}
 data class Customer(val id: String, val name: String, val age: Int)
