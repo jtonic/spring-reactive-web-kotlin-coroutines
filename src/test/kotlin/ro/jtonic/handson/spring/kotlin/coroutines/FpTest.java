f92b49315390eaa13a1e0a511c641655bb45e908
tony <antonel-ernest.pazargic@ing.com>
2024-12-08 13:20:58 +0200

    vavr: basic examples

--- src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/FpTest.java
+++ src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/FpTest.java
@@ -1,10 +1,19 @@
 package ro.jtonic.handson.spring.kotlin.coroutines;
 
+import io.vavr.API;
 import io.vavr.control.Option;
+import io.vavr.control.Try;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FpTest {
 
@@ -36,5 +45,41 @@ public class FpTest {
                 Function.identity()
         );
         System.out.println("res1 = " + res1);
+    }
+
+    @Test
+    void testTry() {
+        var res1 = Try.of(() -> {
+            throw new IOException("Hello");
+        });
+        System.out.println("res1 = " + res1);
+    }
+
+    @Test
+    void testNotThrowingException() {
+        var res1 = Stream.of(1, 2, 3, 4)
+                .map(API.unchecked(this::getFromFile))
+                .filter(Objects::nonNull)
+                .toList();
+        System.out.println("res1 = " + res1);
+        assertThat(res1).hasSize(2);
+        assertThat(res1).isEqualTo(List.of(1, 2));
+    }
+
+    @Test
+    void testThrowsException() {
+        Assertions.assertThatThrownBy(() ->
+                Stream.of(1, 2, 3, 4, 5).map(API.unchecked(this::getFromFile)).toList()
+        ).isInstanceOf(IOException.class);
+    }
+
+    Integer getFromFile(Integer i) throws IOException {
+        if(i >= 5) {
+            throw new IOException("Hello");
+        } else if (i >= 3) {
+            return null;
+        } else {
+            return i;
+        }
     }
 }
