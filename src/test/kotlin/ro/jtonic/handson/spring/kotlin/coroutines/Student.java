83082f3140c8ae03abb658de44300318a118d361
tony <antonel-ernest.pazargic@ing.com>
2024-12-11 20:42:02 +0200

    Lombok and simple validation with either

---
+++ src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/Student.java
@@ -0,0 +1,71 @@
+package ro.jtonic.handson.spring.kotlin.coroutines;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private Integer age;
+    private Street address;
+
+
+    @Getter
+    @AllArgsConstructor
+    static class Street {
+        private String name;
+        private City city;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class City {
+        private String name;
+        private State state;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class State {
+        private String name;
+    }
+
+
+    public static String getStateFromJava7(Student student) {
+        // Java 7
+        if (student != null) {
+            Street street = student.getAddress();
+            if (street != null) {
+                City city = street.getCity();
+                if (city != null) {
+                    State state = city.getState();
+                    if (state != null) {
+                        String stateName = state.getName();
+                        if (stateName != null) {
+                            return stateName;
+                        }
+                        return "unknown";
+                    }
+                    return "unknown";
+                }
+                return "unknown";
+            }
+            return "unknown";
+        }
+        return "unknown";
+    }
+
+    public static String getStateFromJava8(Student student) {
+        Optional<Student> studentOpt = Optional.ofNullable(student);
+        // Java 8
+        return studentOpt
+                .map(Student::getAddress)
+                .map(Street::getCity)
+                .map(City::getState)
+                .map(State::getName)
+                .orElse("unknown");
+    }
+}
---
+++ src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/StudentTest.kt
@@ -0,0 +1,53 @@
+package ro.jtonic.handson.spring.kotlin.coroutines
+
+import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensureNotNull
+import io.kotest.assertions.arrow.core.shouldBeLeft
+import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.assertions.arrow.core.shouldNotBeLeft
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import ro.jtonic.handson.spring.kotlin.coroutines.Student.*
+
+class StudentTest : FreeSpec({
+
+    val student: Student? = Student("Bob", 18, Street("Avenue 12", null))
+
+    "with dummy Option - java 1.7" {
+        getStateFromJava7(student) shouldBe "unknown"
+    }
+
+    "with dummy Option - java 8" {
+        val student = Student("Bob", 18, Street("Avenue 12", null))
+        getStateFromJava8(student) shouldBe "unknown"
+    }
+
+    "with powerful null safe operator" {
+        val state = student?.address?.city?.state?.name ?: "unknown"
+        state shouldBe "unknown"
+    }
+
+    "with either" - {
+        fun getState(student: Student?): Either<String, String> = either {
+            ensureNotNull(student) { "No student" }
+            ensureNotNull(student.address) { "Missing address" }
+            ensureNotNull(student.address.city) { "Missing city" }
+            ensureNotNull(student.address.city.state) { "Missing state" }
+            ensureNotNull(student.address.city.state.name) { "Missing state name" }
+            student.address.city.state.name
+        }
+
+        "student missing city" {
+            val state = getState(student)
+            state shouldBeLeft "Missing city"
+            state shouldNotBeLeft "Missing address"
+        }
+
+        "full info student" {
+            val student2 = Student("Bob", 18, Street("Avenue 12", City("Avenue Park", State("New York"))))
+            val state = getState(student2)
+            state shouldBeRight "New York"
+        }
+    }
+})
