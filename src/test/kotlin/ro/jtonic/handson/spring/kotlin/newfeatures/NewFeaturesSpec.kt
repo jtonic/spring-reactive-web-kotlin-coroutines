fb7b8c1e9f141876d4cbef9a022bd407df2a88f0
tony <antonel-ernest.pazargic@ing.com>
2025-06-23 19:16:21 +0300

    arrow with context

--- src/test/kotlin/ro/jtonic/handson/spring/kotlin/newfeatures/NewFeaturesSpec.kt
+++ src/test/kotlin/ro/jtonic/handson/spring/kotlin/newfeatures/NewFeaturesSpec.kt
@@ -1,41 +1,55 @@
 package ro.jtonic.handson.spring.kotlin.newfeatures
 
+import arrow.core.raise.Raise
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.equals.shouldBeEqual
+import ro.jtonic.handson.spring.kotlin.newfeatures.AppError.UserNotFoundError
 
 class NewFeaturesSpec : FreeSpec({
 
     "context parameters" {
 
-        context(userService: UserService, roleCheckService: RoleCheckService)
-        fun doFindNameByUserId(id: Int, role: String) =
+        context(userService: UserService, roleCheckService: RoleCheckService, raise: Raise<AppError>)
+        fun doFindNameByUserId(id: Int, role: String): User =
+//          ensure(id > 0) { "User ID must be positive" }
             if (roleCheckService.check(role)) {
                 userService.findNameById(id)
             } else {
-                null
+                raise.raise(UserNotFoundError)
             }
 
         val userService: UserService = object : UserService {
-            override fun findNameById(id: Int): String =
-                "Found by userId = $id"
+            override fun findNameById(id: Int): User =
+                User("User by it '$id'")
         }
 
         val roleCheck: RoleCheckService = object : RoleCheckService {
             override fun check(role: String): Boolean = role == "admin"
         }
 
-        context(userService, roleCheck) {
-            val userId = 1;
-            val result = doFindNameByUserId(id = userId, role = "admin")
-            result?.shouldBeEqual("Found by userId = $userId")
+        val userId = 1;
+        val anEither = either {
+            ensure(1 > 0) { "User ID must be positive" }
+            context(userService, roleCheck) {
+                doFindNameByUserId(id = userId, role = "admin")
+            }
         }
+        anEither.shouldBeRight(User("User by it '$userId'"))
     }
 })
 
 interface UserService {
-    fun findNameById(id: Int): String
+    fun findNameById(id: Int): User
 }
 
 interface RoleCheckService {
     fun check(role: String): Boolean
+}
+
+data class User(val name: String)
+
+sealed class AppError(msg: String) {
+    data object UserNotFoundError : AppError("User not found") {}
 }
