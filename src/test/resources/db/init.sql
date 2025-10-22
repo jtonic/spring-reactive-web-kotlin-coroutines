a869b4e14fa99ab059626de7b9b99e52e9dfe021
tony <antonel.pazargic@gmail.com>
2025-10-22 23:43:14 +0300

    Create a MS SQL table

--- src/test/resources/db/init.sql
+++ src/test/resources/db/init.sql
@@ -1,2 +1,44 @@
--- Create new database
 CREATE DATABASE MyAppDB;
+
+-- create a person table in the MyAppDB database default schema
+USE MyAppDB;
+
+CREATE TABLE Person
+(
+    PersonId  INT IDENTITY (1,1) PRIMARY KEY,
+    FirstName NVARCHAR(100),
+    LastName  NVARCHAR(100),
+    Age       INT
+);
+
+-- CREATE LOGIN my_user
+--     WITH PASSWORD = 'StrongP@ssw0rd123';
+--
+-- -- Switch to the new database
+-- USE MyAppDB;
+--
+-- -- Create database user and map it to the login
+-- CREATE USER my_user FOR LOGIN my_user;
+--
+-- -- Grant necessary permissions to the user
+-- ALTER ROLE db_datareader ADD MEMBER my_user;
+-- ALTER ROLE db_datawriter ADD MEMBER my_user;
+--
+-- CREATE SCHEMA MySchema;
+--
+-- -- Grant permissions on the schema
+-- GRANT CREATE TABLE TO my_user;
+-- GRANT ALTER ON SCHEMA::MySchema TO my_user;
+--
+--
+-- -- Create Person table
+-- CREATE TABLE MySchema.Person
+-- (
+--     PersonId  INT IDENTITY (1,1) PRIMARY KEY,
+--     FirstName NVARCHAR(100),
+--     LastName  NVARCHAR(100),
+--     Age       INT
+-- );
+--
+-- -- Grant permissions on the Person table to my_user
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON MySchema.Person TO my_user;
