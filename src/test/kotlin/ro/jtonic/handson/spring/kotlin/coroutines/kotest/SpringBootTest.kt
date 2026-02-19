a2856b11194012f25a3d8ca3b1273dcea2fff9fa
tony <antonel.pazargic@gmail.com>
2026-02-19 23:01:03 +0200

    Update spring boot to latest 3.x version. Add kover-maven-plugin for code

--- src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/kotest/SpringBootTest.kt
+++ src/test/kotlin/ro/jtonic/handson/spring/kotlin/coroutines/kotest/SpringBootTest.kt
@@ -5,15 +5,24 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldNotBe
 import org.springframework.context.ApplicationContext
+import ro.jtonic.handson.spring.kotlin.coroutines.Input
 import ro.jtonic.handson.spring.kotlin.coroutines.KotlinSpringIntegrationTest
+import ro.jtonic.handson.spring.kotlin.coroutines.UseCase
 
 @KotlinSpringIntegrationTest
 @ApplyExtension(extensions = [SpringExtension::class])
 class SpringBootTest(
-    val appCtx: ApplicationContext
+    val appCtx: ApplicationContext,
+    val runFlow: UseCase<Input, Unit>,
 ) : FreeSpec({
 
         "test spring context loads" {
             appCtx shouldNotBe null
         }
+
+    "test coverage for spring beans" - {
+        "runFlow" {
+            runFlow(Input(1))
+        }
+    }
 })
